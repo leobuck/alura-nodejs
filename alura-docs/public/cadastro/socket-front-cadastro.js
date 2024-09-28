daaee3790048cf4af90ad56427924316fab029fd
leobuck <leobuck12@gmail.com>
2024-09-28 17:31:02 -0300

    Cadastrando no banco de dados

--- alura-docs/public/cadastro/socket-front-cadastro.js
+++ alura-docs/public/cadastro/socket-front-cadastro.js
@@ -4,4 +4,16 @@ function emitirCadastrarUsuario(dados) {
   socket.emit("cadastrar_usuario", dados);
 }
 
+socket.on("cadastro_sucesso", () => {
+  alert("Cadastro realizado com sucesso!");
+});
+
+socket.on("cadastro_erro", () => {
+  alert("Erro no cadastro!");
+});
+
+socket.on("usuario_ja_existente", () => {
+  alert("Usuário já existe!");
+});
+
 export { emitirCadastrarUsuario };
--- alura-docs/servidor/db/dbConnect.js
+++ alura-docs/servidor/db/dbConnect.js
@@ -5,15 +5,18 @@ const cliente = new MongoClient(
 );
 
 let documentosColecao;
+let usuariosColecao;
 
 try {
   await cliente.connect();
 
   const db = cliente.db("alura-websockets");
   documentosColecao = db.collection("documentos");
+  usuariosColecao = db.collection("usuarios");
+
   console.log("Conectado ao banco de dados com sucesso!");
 } catch (error) {
   console.log(error);
 }
 
-export { documentosColecao };
+export { documentosColecao, usuariosColecao };
