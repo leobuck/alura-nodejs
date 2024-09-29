710c508cbc8f85a334be661e0cf12bde61402f84
leobuck <leobuck12@gmail.com>
2024-09-29 10:04:45 -0300

    Enviando dados de login

--- alura-docs/servidor/socket-back.js
+++ alura-docs/servidor/socket-back.js
@@ -1,10 +1,12 @@
 import registrarEventosCadastro from "./registrarEventos/cadastro.js";
 import registrarEventosDocumento from "./registrarEventos/documento.js";
 import registrarEventosInicio from "./registrarEventos/inicio.js";
+import registrarEventosLogin from "./registrarEventos/login.js";
 import io from "./servidor.js";
 
 io.on("connection", (socket) => {
   registrarEventosInicio(socket, io);
   registrarEventosDocumento(socket, io);
   registrarEventosCadastro(socket, io);
+  registrarEventosLogin(socket, io);
 });
