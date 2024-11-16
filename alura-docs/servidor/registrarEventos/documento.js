4d1f52a9df2a1968d50a5808f65ee28b6873e992
leobuck <leobuck12@gmail.com>
2024-11-16 08:26:21 -0300

    Lidando com entrada de usuário

--- alura-docs/servidor/registrarEventos/documento.js
+++ alura-docs/servidor/registrarEventos/documento.js
@@ -5,14 +5,18 @@ import {
 } from "../db/documentosDb.js";
 
 function registrarEventosDocumento(socket, io) {
-  socket.on("selecionar_documento", async (nomeDocumento, devolverTexto) => {
-    socket.join(nomeDocumento);
+  socket.on(
+    "selecionar_documento",
+    async ({ nomeDocumento, nomeUsuario }, devolverTexto) => {
+      console.log(nomeUsuario);
+      socket.join(nomeDocumento);
 
-    const documento = await encontrarDocumento(nomeDocumento);
-    if (documento) {
-      devolverTexto(documento.texto);
+      const documento = await encontrarDocumento(nomeDocumento);
+      if (documento) {
+        devolverTexto(documento.texto);
+      }
     }
-  });
+  );
 
   socket.on("texto_editor", async ({ texto, nomeDocumento }) => {
     const atualizacao = await atualizaDocumento(nomeDocumento, texto);
